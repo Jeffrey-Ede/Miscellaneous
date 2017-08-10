bd8fb531651cd08ffd97b9d74d585467993601f5
Jeffrey Ede <jeffreyede1@outlook.com>
2017-08-10 13:22:54 +0100

    Add files via upload
    Multiplication tables. However, all the values are cubed.

---
+++ cubed_multiples.php
@@ -0,0 +1,17 @@
+<html>
+<head><title>Multiplication Tables Cubed</title></head>
+<body>
+
+<?php
+	# operator
+	print "Multiplication tables; however, all the values are cubed.<br />";
+		for($i=1; $i<10; $i++){
+			for($j=1; $j<10; $j++){
+				echo $val = pow($i*$j, 3);
+			}
+			echo "<br />";
+		}
+?>
+
+</body>
+</html>
