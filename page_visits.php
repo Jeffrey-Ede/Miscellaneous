bf7295b676d9a1c8a363bcda86837f8d9af5b412
Jeffrey Ede <jeffreyede1@outlook.com>
2017-08-11 09:52:46 +0100

    Add files via upload
    Counts the number of times a page has been visited using a PHP session.

---
+++ page_visits.php
@@ -0,0 +1,30 @@
+<?php
+   session_start();
+   
+   if( isset( $_SESSION['counter'] ) ) {
+      $_SESSION['counter'] += 1;
+   }else {
+      $_SESSION['counter'] = 1;
+   }
+	
+   $msg = "You have visited this page ".  $_SESSION['counter'];
+   $msg .= " times this session.";
+?>
+
+<html>
+   
+   <head>
+      <title>Setting up a PHP session</title>
+   </head>
+   
+   <body>
+      <?php  echo ( $msg ); ?>
+   </body>
+   
+</html>
+
+
+<?php
+   // Uncomment line below to destroy session
+   //session_destroy();
+?>
