acb1961a930ab26b85bf8ef43667886c46de1a03
Jeffrey Ede <jeffreyede1@outlook.com>
2017-08-14 15:49:28 +0100

    Add files via upload

--- conditionals.pl
+++ conditionals.pl
@@ -1,5 +1,6 @@
 # Perl doesn't know how great chocolate is...
-# The result is "interesting..."if("Cookies">"Chocolate"){
+# The result is "interesting..."
+# Need to use special operators like lt or gt for stringwise comparisonif("Cookies">"Chocolate"){
 	print "But I like chocolate...\n";
 }
 elsif("Cookies"<"Chocolate"){
