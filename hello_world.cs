562e04a3bd16023d3e4bde6e8150f97667b2af31
Jeffrey Ede <jeffreyede1@outlook.com>
2017-08-23 14:54:49 +0100

    Add files via upload

---
+++ hello_world.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Jeffrey Ede
+ * Date: 23/08/2017
+ * Time: 14:50
+ * 
+ */
+ 
+using System;
+namespace HelloWorldApp
+{
+	class Hello_World
+	{
+		static void Main(string[] args)
+		{
+			// Print "Hello world!"
+			Console.WriteLine("Hello world!");
+			Console.ReadKey();
+		}
+	}
+}
